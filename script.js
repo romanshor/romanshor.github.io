67cdb023771ae99187a05a9d269cc4f94e3758e5
Roman Shor <roman.shor@ucalgary.ca>
2024-06-08 14:51:27 -0600

    Updated to v2.0 of website

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    function setActiveLink() {
+        let index = sections.length;
+
+        while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+        navLinks.forEach((link) => link.classList.remove("active"));
+        navLinks[index].classList.add("active");
+    }
+
+    window.addEventListener("scroll", setActiveLink);
+});
